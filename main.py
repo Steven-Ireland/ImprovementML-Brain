b4111ea4b8df29ce522c181ab6194e0732089af9
Steven Ireland <scientastersteve@gmail.com>
2021-04-06 20:58:43 -0400

    working v2 robot shoe edition

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import socket
 from gym import spaces
+from operator import itemgetter
+import json
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import SubprocVecEnv #, VecNormalize
 import numpy as np
@@ -21,13 +23,13 @@ def make_env(rank):
 
         ## TODO: Pull the space inits into the env passed through this?
         greeting = clientsocket.recv(BUF_SIZE).decode()
-        print(f"> {greeting}")
+        observation_size, action_size = itemgetter('observationSize', 'actionSize')(json.loads(greeting))
 
         # See observation.jsonc for the spec        
         ## TODO: Make the space size dynamically driven from the unity scene
-        high = np.array([np.inf] * 15)
+        high = np.array([np.inf] * observation_size)
         observation_space = spaces.Box(-high, high, dtype='float32')
-        action_space = spaces.Box(np.array([-1] * 2), np.array([1] * 2), dtype='float32')
+        action_space = spaces.Box(np.array([-1] * action_size), np.array([1] * action_size), dtype='float32')
         
         env = game_env.GameEnv(clientsocket, observation_space, action_space)
         obs = env.reset()
