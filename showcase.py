aa2e5c33624302af52978f9697669a45db46790c
Steven Ireland <scientastersteve@gmail.com>
2021-06-27 11:02:58 -0400

    remove socket_server, add back showcase

---
+++ showcase.py
@@ -0,0 +1,22 @@
+from gym import spaces
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import SubprocVecEnv #, VecNormalize
+import numpy as np
+import time
+
+import game_env
+
+if __name__ == "__main__":
+    env = game_env.make_env(0)()
+
+    model = PPO.load("isitworking9")
+
+    obs = env.reset()
+
+    for i in range(10_000):
+        action, _state = model.predict(obs, deterministic=True)
+        obs, reward, done, info = env.step(action)
+        time.sleep(1.0 / 60.0)
+
+        if done:
+            obs = env.reset()
